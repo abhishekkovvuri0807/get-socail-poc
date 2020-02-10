d8f161fbffc1f6719256e970b7087ed2532fa18d
Abhishek Kovvuri <abhishek.kovvuri@gmail.com>
2020-02-10 02:29:22 -0500

    Updates

---
+++ precache-manifest.511fb2345837c3b3111184258a79f72a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70a5b1e4f795f858d8bd69e1217e3e7e",
+    "url": "/get-social-poc/index.html"
+  },
+  {
+    "revision": "9ae42722c0ec17e183dc",
+    "url": "/get-social-poc/static/css/main.1cc6f2b1.chunk.css"
+  },
+  {
+    "revision": "b7493df3e388ecb4e12b",
+    "url": "/get-social-poc/static/js/2.68e0b72c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/get-social-poc/static/js/2.68e0b72c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ae42722c0ec17e183dc",
+    "url": "/get-social-poc/static/js/main.fa050cc1.chunk.js"
+  },
+  {
+    "revision": "e2fe06a68a30d190c670",
+    "url": "/get-social-poc/static/js/runtime-main.e83977fc.js"
+  }
+]);
